f2a89981986d0888a5394ed8d14ae23a3b9d08a8
COLDRAMEN1221 <mhillyer27@germantownfriends.org>
2026-09-17 18:04:49 -0400

    added simple win checking

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from simple_graphics import *
 import random
 
-board_height = 8
-board_width = 8 
+board_height = 6
+board_width = 7 
 chip_size = 12 
 
 def draw_board(board: list[list]):
@@ -24,12 +24,48 @@ for y in range(board_height):
     cells.append(row)
 
 color = 'red'
+
+def check_board():
+    for current_color in ['yellow', 'red']:
+        for r in range(board_height):
+            for c in range(board_width):
+                if cells[r][c].color != current_color:
+                    continue
+
+                if c + 3 < board_width and all(cells[r][c + i].color == current_color for i in range(4)):
+                    return current_color
+                if r + 3 < board_height and all(cells[r + i][c].color == current_color for i in range(4)):
+                    return current_color
+                if r + 3 < board_height and c + 3 < board_width and all(cells[r + i][c + i].color == current_color for i in range(4)):
+                    return current_color
+                if r - 3 >= 0 and c + 3 < board_width and all(cells[r - i][c + i].color == current_color for i in range(4)):
+                    return current_color
+    return 0
+
+won = False
+
 @on_click
 def drop_chip():
     global color
+    global won
+
+    if won:
+        return
+
     for y in range(board_height):
         for x in range(board_width):
-             if cells[y][x].is_obj_over(mouse.x, mouse.y) and (cells[y][x]).color == 'gray':
-                 (cells[y][x]).color = color
-                 color = 'yellow' if color == 'red' else 'red'
+            if cells[y][x].is_obj_over(mouse.x, mouse.y) and cells[y][x].color == 'gray':
+                for i in range(board_height - 1, -1, -1):
+                    if cells[i][x].color == 'gray':
+                        player = color
+                        cells[i][x].color = player
+                        winner = check_board()
+                        if winner != 0:
+                            Text(0, 0, f'{winner} WINS', winner)
+                            won = True
+                        else:
+                            color = 'yellow' if player == 'red' else 'red'
+                        return
+                return
+                 
 run(width=400, height=400, caption="My Graphics App")
